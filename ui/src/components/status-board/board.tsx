756f9905b27c832f6800727b36389936d20c5aef
Bruno Carvalho <brunoeee@gmail.com>
2020-03-03 11:43:53 +0100

    fix mini features

--- ui/src/components/status-board/board.tsx
+++ ui/src/components/status-board/board.tsx
@@ -2,22 +2,28 @@
 // Copyright (C) 2018-2019 Prusa Research s.r.o. - www.prusa3d.com
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { h, Fragment } from 'preact';
-import { Text } from 'preact-i18n';
+import { h, Fragment } from "preact";
+import { Text } from "preact-i18n";
 import { numberFormat, formatTime, formatTimeEnd } from "../utils/index";
 
 interface StatusBoardItemProps {
-  id: string,
-  title: string,
-  value: string | number
+  id: string;
+  title: string;
+  value: string | number;
 }
 export interface StatusBoardTableProps {
-  readonly remaining_time: number,
-  readonly printing_time: number,
-  readonly current_layer: number,
-  readonly total_layers: number,
-  readonly remaining_material?: number,
-  readonly consumed_material?: number
+  readonly remaining_time?: number;
+  readonly printing_time?: number;
+  readonly current_layer?: number;
+  readonly total_layers?: number;
+  readonly remaining_material?: number;
+  readonly consumed_material?: number;
+  readonly pos_z_mm?: number;
+  readonly printing_speed?: number;
+  readonly flow_factor?: number;
+  readonly print_dur?: number;
+  readonly time_est?: string;
+  readonly filament_status?: string;
 }
 
 const StatusBoardItem = (props: StatusBoardItemProps) => {
@@ -33,7 +39,7 @@ const StatusBoardItem = (props: StatusBoardItemProps) => {
   );
 };
 
-export const StatusBoardTable = ({
+export const StatusBoardSL1 = ({
   remaining_time,
   printing_time,
   current_layer,
@@ -44,25 +50,97 @@ export const StatusBoardTable = ({
   return (
     <Fragment>
       <div class="columns">
-        <StatusBoardItem id="remaining-time" title="Remaining time" value={remaining_time > 0 ? formatTime(remaining_time) : "0 min"} />
-        {
-          process.env.PRINTER != "Original Prusa Mini" &&
-          <StatusBoardItem id="estimated-end" title="Estimated end" value={remaining_time > 0 ? formatTimeEnd(remaining_time) : "00:00"} />
-        }
-        <StatusBoardItem id="printing-time" title="Printing time" value={printing_time > 0 ? formatTime(printing_time) : "0 min"} />
-        {
-          process.env.PRINTER == "Original Prusa Mini" &&
-          <StatusBoardItem id="layer" title="Layer" value={total_layers > 0 ? `${current_layer}/${total_layers}` : "0/0"} />
-        }
+        <StatusBoardItem
+          id="remaining-time"
+          title="Remaining time"
+          value={remaining_time > 0 ? formatTime(remaining_time) : "0 min"}
+        />
+        <StatusBoardItem
+          id="estimated-end"
+          title="Estimated end"
+          value={remaining_time > 0 ? formatTimeEnd(remaining_time) : "00:00"}
+        />
+        <StatusBoardItem
+          id="printing-time"
+          title="Printing time"
+          value={printing_time > 0 ? formatTime(printing_time) : "0 min"}
+        />
       </div>
-      {
-        process.env.PRINTER != "Original Prusa Mini" &&
-        <div class="columns">
-          <StatusBoardItem id="layer" title="Layer" value={total_layers > 0 ? `${current_layer}/${total_layers}` : "0/0"} />
-          <StatusBoardItem id="remaining-resin" title="Remaining resin" value={`${numberFormat(remaining_material)} ml`} />
-          <StatusBoardItem id="consumed-resin" title="Consumed resin" value={`${numberFormat(consumed_material)} ml`} />
-        </div>
-      }
+      <div class="columns">
+        <StatusBoardItem
+          id="layer"
+          title="Layer"
+          value={total_layers > 0 ? `${current_layer}/${total_layers}` : "0/0"}
+        />
+        <StatusBoardItem
+          id="remaining-resin"
+          title="Remaining resin"
+          value={`${numberFormat(remaining_material)} ml`}
+        />
+        <StatusBoardItem
+          id="consumed-resin"
+          title="Consumed resin"
+          value={`${numberFormat(consumed_material)} ml`}
+        />
+      </div>
     </Fragment>
   );
+};
+
+export const StatusBoardMini = ({
+  pos_z_mm,
+  printing_speed,
+  flow_factor,
+  print_dur,
+  time_est,
+  filament_status
+}: StatusBoardTableProps) => {
+  const available = (value, unit = null) =>
+    value ? value + (unit ? " " + unit : "") : "NA";
+  return (
+    <Fragment>
+      <div class="columns">
+        <StatusBoardItem
+          id="pos_z_mm"
+          title="Z-Height"
+          value={available(pos_z_mm, "mm")}
+        />
+        <StatusBoardItem
+          id="printing-speed"
+          title="Printing Speed"
+          value={available(printing_speed, "%")}
+        />
+        <StatusBoardItem
+          id="flow-factor"
+          title="Printing Flow"
+          value={available(flow_factor)}
+        />
+      </div>
+      <div class="columns">
+        <StatusBoardItem
+          id="time-est"
+          title="Remaining time"
+          value={available(print_dur)}
+        />
+        <StatusBoardItem
+          id="print-dur"
+          title="Printing time"
+          value={available(time_est)}
+        />
+        <StatusBoardItem
+          id="filament-status"
+          title="Filament sensor"
+          value={available(filament_status)}
+        />
+      </div>
+    </Fragment>
+  );
+};
+
+export const StatusBoardTable = (props: StatusBoardTableProps) => {
+  if (process.env.PRINTER == "Original Prusa Mini") {
+    return <StatusBoardMini {...props} />;
+  } else {
+    return <StatusBoardSL1 {...props} />;
+  }
 };
