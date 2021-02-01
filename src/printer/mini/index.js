52ce0759980d9162b56261793f3213151502fd6b
Bruno Carvalho <bruno.carvalho@prusa3d.cz>
2021-02-01 16:50:16 +0100

    a server mock for api

--- src/printer/mini/index.js
+++ src/printer/mini/index.js
@@ -9,39 +9,45 @@ import dashboard from "../../views/dashboard.html";
 import temperature from "../../views/temperature.html";
 
 const mini = {
-    routes: [
-        { path: 'dashboard', html: dashboard, module: Dashboard },
-        { path: 'temperature', html: temperature, module: Temperature },
-    ],
-    init: () => {
-        console.log("Init Printer API");
-        initTemperatureGraph();
-    },
-    update: () => {
-        console.log("Update Printer API")
-        updateTemperatureGraph();
-    }
-}
+  routes: [
+    { path: "dashboard", html: dashboard, module: Dashboard },
+    { path: "temperature", html: temperature, module: Temperature },
+  ],
+  init: () => {
+    console.log("Init Printer API");
+    initTemperatureGraph();
+  },
+  update: () => {
+    console.log("Update Printer API");
+    updateTemperatureGraph();
+  },
+};
 
 const initTemperatureGraph = () => {
-    const maxStep = 15;
-    const maxTemp = 300;
-    const now = new Date().getTime();
+  const maxStep = 15;
+  const maxTemp = 300;
+  const now = new Date().getTime();
 
-    let map = new Map([
-        ['temp-line-blue', graph.generateRandomGraph(now, 0, maxTemp, maxStep)],
-        ['temp-line-orange', graph.generateRandomGraph(now, 0, maxTemp, maxStep)],
-    ]);
+  let map = new Map([
+    ["temp-line-blue", graph.generateRandomGraph(now, 0, maxTemp, maxStep)],
+    ["temp-line-orange", graph.generateRandomGraph(now, 0, maxTemp, maxStep)],
+  ]);
 
-    graph.init(map, maxTemp, maxStep);
-    graph.render();
-}
+  graph.init(map, maxTemp, maxStep);
+  graph.render();
+};
 
 const updateTemperatureGraph = () => {
-    const now = new Date().getTime();
-    graph.update('temp-line-blue', [now, graph.generateNextTemp('temp-line-blue')]);
-    graph.update('temp-line-orange', [now, graph.generateNextTemp('temp-line-orange')]);
-    graph.render();
-}
+  const now = new Date().getTime();
+  graph.update("temp-line-blue", [
+    now,
+    graph.generateNextTemp("temp-line-blue"),
+  ]);
+  graph.update("temp-line-orange", [
+    now,
+    graph.generateNextTemp("temp-line-orange"),
+  ]);
+  graph.render();
+};
 
 export default mini;
