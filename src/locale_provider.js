b7badb2dacee06a236a2a84cd177f9459444aebf
Elias Kartashov <elias@lowenware.com>
2021-02-15 14:09:10 +0100

    Improve localization functions

--- src/locale_provider.js
+++ src/locale_provider.js
@@ -1,7 +1,7 @@
 /** Language that is tranlated from */
-const DEFAULT_LANGUAGE = 'en_US';
+const DEFAULT_LANGUAGE = "en";
 /** File that contains all translations */
-const locales = require('./locales/locales.json');
+const locales = require("./locales/locales.json");
 /** All known languages */
 const languages = locales.langs;
 /** All known texts */
@@ -23,46 +23,42 @@ export function setLanguage(language) {
     langIndex = languages.indexOf(lang);
 }
 
-/** Translate string into the current language.
- * If you need to pass some parameters, use %(paramName) syntax and pass { paramName: value } into params
- * @param id Element id
- * @param {string} label The String to translate from. Pass everything in string. Do not use variables!
- * @param params Object that contains parameters. For example { paramName: value }
+/** Translate text into the current language. If you need to pass some parameters,
+ * use %(paramName) in text and pass { paramName: value } into parameters. Use query
+ * parameter if you need to pass result into HTML element. "query" parameter is reserved
+ * and is not passed to the text.
+ * @param {string} text The text to translate from. Must be simple strin. Do not use variables!
+ * @param {{query?:any}|undefined} parameters Object that contains parameters.
+ * query = where to insert the translation; Other parameters will be passed to given string
+ * @return {string}
 */
-export function translateById(id, label, params) {
-    let element = document.getElementById(id);
-    if (element) {
-        element.innerHTML = translate(label, params);
-    } else {
-        console.warn(`Element with id "${id}" not found`);
-    }
-}
-
-/** Translate string into the current language.
- * If you need to pass some parameters, use %(paramName) syntax and pass { paramName: value } into second parameter
- * @param {string} str The String to translate from. Pass everything in string. Do not use variables!
- * @param parameters Object that contains parameters. For example { paramName: value }
-*/
-export function translate (str, parameters) {
+export function translate(text, parameters) {
     let word = null;
 
-    if (languages.includes(lang) && str in texts) {
-        word = texts[str][langIndex];
+    if (languages.includes(lang) && text in texts) {
+        word = texts[text][langIndex];
     }
 
     if (word === null) {
         if (lang !== DEFAULT_LANGUAGE) {
-            console.warn(`[${lang}] missing translation for "${str.split('\n').join('\\n')}"`);
+            console.warn(`[${lang}] missing translation for "${text.split("\n").join("\\n")}"`);
         }
-        word = str;
+        word = text;
     }
 
-    if (parameters) {
+    // Check for parameters, ignore query
+    const haveParams = typeof parameters === "object" && (
+        Object.keys(parameters).length > 1 || !("query" in parameters)
+    );
+
+    if (haveParams) {
         let editedWord = word;
         let regex = /%\((.*?)\)/g; // search for %()
         let match;
         while (match = regex.exec(word)) {
-            let paramName = word.substr(match.index + 2, match[0].length - 3); // %(parameter) => parameter
+            // %(parameter) => parameter
+            let paramName = word.substr(match.index + 2, match[0].length - 3);
+            if (paramName === "query") continue;
             if (paramName in parameters) {
                 let param = parameters[paramName];
                 // Checks for float digis suffix
@@ -74,11 +70,24 @@ export function translate (str, parameters) {
                     editedWord = editedWord.replace(match[0], param);
                 }
             } else {
-                console.warn(`missing parameter [${paramName}] in translation for ${str}.`);
+                console.warn(`missing parameter [${paramName}] in translation for ${text}.`);
             }
         }
-        return editedWord;
+        word = editedWord;
     }
 
+    if (parameters && parameters["query"])
+        setText(parameters.query, word);
+
     return word;
+}
+
+function setText(query, text) {
+    let element = document.querySelector(query);
+    if (element) {
+        element.innerHTML = text;
+        console.log(element);
+    } else {
+        console.warn(`cannot find element with "${query}" query`);
+    }
 }
