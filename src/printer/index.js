64f85ba3097b5a0a598239d498fb33863e76c3f3
Ilja Kartašov <ik@lowenware.com>
2022-06-06 08:53:41 +0200

    Remove deprecated WITH_FONT configuration parameter

--- src/printer/index.js
+++ src/printer/index.js
@@ -2,13 +2,6 @@
 // Copyright (C) 2021 Prusa Research a.s. - www.prusa3d.com
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-// NOTE: globally disable custom font for now
-// if (process.env.WITH_FONT) {
-//   require("../atlas-grotesk-web.css");
-//}
-
-import "../fonts.css";
-
 const printer = (() => {
   if (process.env.PRINTER_TYPE === "sla") return require("./sla");
   if (process.env.PRINTER_TYPE === "fdm") return require("./fdm");
