205b4e5e9690608e1d419850c2c02f30def650e0
Michal 'vorner' Vaner <vorner@vorner.cz>
2022-07-15 15:46:34 +0200

    Enable printing features for Prusa MINI

--- config.mini.js
+++ config.mini.js
@@ -10,14 +10,14 @@ module.exports = (env, args) => {
         WITH_SETTINGS: false,
         WITH_CONTROLS: false,
         WITH_REMOTE_UPLOAD: false,
-        WITH_START_PRINT_AFTER_UPLOAD: false,
+        WITH_START_PRINT_AFTER_UPLOAD: true,
         WITH_SERIAL: false,
         WITH_CONNECTION: false,
         WITH_LOGS: false,
         WITH_FONT: false,
         WITH_EMBEDDED_SVGS: true,
         WITH_COMMAND_SELECT: false,
-        WITH_PRINT_BUTTON: false,
+        WITH_PRINT_BUTTON: true,
         ...env,
     };
     return webpackConfig(config, args);
