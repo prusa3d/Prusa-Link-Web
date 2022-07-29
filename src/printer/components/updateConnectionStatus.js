ea1d5bd281f91634c9494d556899464317caead9
Ilja Kartašov <ik@lowenware.com>
2022-07-29 17:34:34 +0200

    Add offline screen and remove toast messages for connection errors

--- src/printer/components/updateConnectionStatus.js
+++ src/printer/components/updateConnectionStatus.js
@@ -1,10 +1,24 @@
 import { setHidden, setVisible } from "../../helpers/element";
 import { translate } from "../../locale_provider";
+import { modal } from "./modal";
+
+const failuresBeforeOfflineScreen = process.env.WITH_FAILURES_BEFORE_OFFLINE_SCREEN;
+let casesCount = 0;
+let offlineScreen = null;
 
 /** Updates connection status in telemetry. */
 const updateConnectionStatus = ({ connection, isConnected }) => {
   if (isConnected) {
     const states = connection?.states || {"printer": {"ok": true, "message": ""}};
+
+    if (offlineScreen) {
+      if (casesCount < failuresBeforeOfflineScreen) {
+        casesCount += 1;
+      } else {
+        offlineScreen.close();
+        casesCount = 0;
+      }
+    }
 
     for (const name in states) {
       const { ok, message } = states[name];
@@ -18,7 +32,44 @@ const updateConnectionStatus = ({ connection, isConnected }) => {
     if (msgElm)
       msgElm.innerHTML = translate("conn.err-conn");
     setVisible(document.getElementById("conn-status-printer"));
+
+    if (!offlineScreen) {
+      if (casesCount < failuresBeforeOfflineScreen) {
+        casesCount += 1;
+      } else {
+        openOfflineScreen();
+        casesCount = 0;
+      }
+    }
   }
+}
+
+const openOfflineScreen = () => {
+  const createOfflineScreen = (close) => {
+    const template = document.getElementById("offline-screen");
+    const node = document.importNode(template.content, true);
+    ["not-responsing", "please-wait"].forEach(
+      id => {
+        const label = node.getElementById(`offline-screen.${id}`);
+        if (label) {
+          label.innerHTML = translate(`msg.offline.${id}`);
+        }
+      }
+    );
+    offlineScreen = {
+      node,
+      close: () => {
+        close();
+        offlineScreen = null;
+      }
+    };
+    return node;
+  };
+  modal((close) => createOfflineScreen(close), {
+    timeout: 0,
+    closeOutside: false,
+    className: 'offline-screen'
+  });
 }
 
 export default updateConnectionStatus;
