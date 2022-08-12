4d1038087c21aa5992c89a006dd85d986eb51d68
Ilja Kartašov <ik@lowenware.com>
2022-08-12 13:07:52 +0200

    Update words extractor to populate lang files with new values

--- tools/preprocessing/extract-words.js
+++ tools/preprocessing/extract-words.js
@@ -3,6 +3,7 @@ const path = require("path");
 const {getWordsFromHTML, getWordsFromJS} = require('../loaders/shared.js');
 
 const WORDS_FILE = path.resolve(__dirname, "../../src/locales/c.json");
+const WORDS_DIR = path.resolve(__dirname, "../../src/locales/");
 
 const JS_SRC = 'src/';
 const HTML_SRC = 'templates/';
@@ -24,27 +25,42 @@ const readWordsFromFiles = (files, getWordsFrom) => {
     return files.flatMap(filePath => getWordsFrom(`${fs.readFileSync(filePath)}`));
 }
 
+const stringifyWords = (words) => {
+    return JSON.stringify(words, undefined, 2);
+}
+
 function main() {
+    let isSync = true;
     const jsFiles = readDirRecursive(JS_SRC, ".js");
     const htmlFiles = readDirRecursive(HTML_SRC, ".html");
+    const wordsFiles = readDirRecursive(WORDS_DIR, ".json").filter(fn => !fn.endsWith('c.json'));
 
     const initial = (fs.existsSync(WORDS_FILE)) ? `${fs.readFileSync(WORDS_FILE)}` : null;
-    const words = JSON.stringify(
-        Object.fromEntries([
-            ...readWordsFromFiles(jsFiles, getWordsFromJS),
-            ...readWordsFromFiles(htmlFiles, getWordsFromHTML),
-        ]
-            .filter(w => !!w)
-            .sort()
-            .map(key => [key, ""])
-        ),
-        undefined,
-        2
+    const words = Object.fromEntries([
+        ...readWordsFromFiles(jsFiles, getWordsFromJS),
+        ...readWordsFromFiles(htmlFiles, getWordsFromHTML),
+    ]
+        .filter(w => !!w)
+        .sort()
+        .map(key => [key, ""])
     );
 
-    fs.writeFileSync(WORDS_FILE, words);
+    wordsFiles.forEach(fileName => {
+        // const filePath = path.join(WORDS_DIR, fileName);
+        const langWords = JSON.parse(fs.readFileSync(fileName));
+        Object.keys(words).forEach(key => {
+            if (!(key in langWords)) {
+                langWords[key] = "";
+                isSync = false;
+            }
+        })
+        fs.writeFileSync(fileName, stringifyWords(langWords));
+    });
 
-    if (initial !== words) {
+    const result = stringifyWords(words);
+    fs.writeFileSync(WORDS_FILE, result);
+
+    if (!isSync) {
         console.warn("Words file was not up to date!");
         return 1;
     }
