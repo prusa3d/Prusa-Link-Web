de1cd1925e3c5ce918d4be5ad48abef351989ea3
Ilja Kartašov <ik@lowenware.com>
2022-11-23 13:21:41 +0100

    Improve UX of the dropdown component

--- src/printer/components/dropdown/index.js
+++ src/printer/components/dropdown/index.js
@@ -9,13 +9,15 @@ export class Dropdown {
   set value(val) {
     this._value = val;
     this.updateLabel();
-  };
+  }
 
   get value() {
     return this._value;
-  };
+  }
 
   constructor(btn, label, ul) {
+    const dropdown = this;
+
     /** @type {HTMLElement} */
     this._label = label;
     /** @type {HTMLElement} */
@@ -24,20 +26,78 @@ export class Dropdown {
     this._value = label.innerHTML;
     /** @type {boolean} */
     this.is_open = false;
+    this._options = [];
+
+    this._onKeyDown = (event) => {
+      if (!dropdown.is_open) {
+        return;
+      }
+
+      switch (event.key.toLowerCase()) {
+        // close the dropdown on escape
+        case "escape":
+          dropdown.close();
+          return;
+
+        // select next node on arrow down
+        case "arrowdown":
+          let selectThis = false;
+          for (const li of this._ul.childNodes) {
+            if (selectThis) {
+              this._highlight(li);
+              break;
+            }
+            if (li.classList?.contains("select")) {
+              selectThis = true;
+            }
+          }
+          break;
+
+        // select previous node on arrow up
+        case "arrowup":
+          let previous = null;
+          for (const li of this._ul.childNodes) {
+            if (li.classList?.contains("select")) {
+              if (previous) {
+                this._highlight(previous);
+              }
+              break;
+            }
+            previous = li;
+          }
+          break;
+
+        // select current node
+        case "enter":
+          for (const li of this._ul.childNodes) {
+            if (li.classList.contains("select")) {
+              const value = li.innerText;
+              this.value = value;
+              if (this.onselect) this.onselect(value);
+              this.close();
+              break;
+            }
+          }
+          break;
+      }
+      event.preventDefault();
+    };
+
+    this._onClick = (event) => {
+      const target = event.target.parentNode;
+      if (!dropdown.is_open) {
+        return true;
+      }
+      if (target !== btn) {
+        dropdown.close();
+      }
+    };
+
+    window.addEventListener("keydown", this._onKeyDown, false);
+    window.addEventListener("click", this._onClick, false);
 
     /** @type {(value:String) => void} **/
     this.onselect = undefined;
-
-    const closeClick = () => {
-      this.close();
-      window.removeEventListener("click", closeClick);
-    };
-
-    // Fix bug with instant close / or multiple dropdowns opened
-    const closeReady = () => {
-      window.addEventListener("click", closeClick);
-      window.removeEventListener("pointerup", closeReady);
-    }
 
     btn.onclick = (e) => {
       e.preventDefault();
@@ -45,9 +105,23 @@ export class Dropdown {
         this.close();
       } else {
         this.open();
-        window.addEventListener("pointerup", closeReady)
       }
     };
+  }
+
+  _highlight(option) {
+    this._ul.childNodes.forEach((o) => {
+      if (o === option) {
+        o.classList.add("select");
+      } else {
+        o.classList.remove("select");
+      }
+    });
+  }
+
+  destructor() {
+    window.removeEventListener("keypress", this._onKeyPress);
+    window.removeEventListener("click", this._onClick);
   }
 
   /** Replace input element with dropdown.
@@ -61,12 +135,12 @@ export class Dropdown {
     let rootElement = getElement(root);
 
     const template = document.getElementById("dropdown-template");
-    const select = rootElement.getAttribute("data-type") === "dropdown"
-      ? rootElement
-      : rootElement.querySelector('select[data-type="dropdown"]');
+    const select =
+      rootElement.getAttribute("data-type") === "dropdown"
+        ? rootElement
+        : rootElement.querySelector('select[data-type="dropdown"]');
 
-    if (!select)
-      return undefined;
+    if (!select) return undefined;
 
     select.after(document.importNode(template.content, true));
     const dropdown = select.nextElementSibling;
@@ -88,18 +162,7 @@ export class Dropdown {
   }
 
   setOptions(options) {
-    options.forEach(option => {
-      const li = document.createElement("li");
-      li.innerHTML = option;
-      li.onclick = () => {
-        this.select(option);
-        this.value = option;
-        if (this.onselect)
-          this.onselect(option);
-        this.close();
-      }
-      this._ul.appendChild(li);
-    });
+    this._options = options;
   }
 
   updateLabel() {
@@ -111,13 +174,32 @@ export class Dropdown {
   }
 
   open() {
-    if (!this._ul.classList.contains("open"))
+    if (!this._ul.classList.contains("open")) {
+      this._options.forEach((option) => {
+        const li = document.createElement("li");
+        if (option === this._value) {
+          li.className = "select selected";
+        }
+        li.innerText = option;
+        li.onclick = () => {
+          this.select(option);
+          this.value = option;
+          if (this.onselect) this.onselect(option);
+          this.close();
+        };
+        li.onmouseover = () => this._highlight(li);
+        this._ul.appendChild(li);
+      });
       this._ul.classList.add("open");
+    }
     this.is_open = true;
-  };
+  }
 
   close() {
     this._ul.classList.remove(["open"]);
+    while (this._ul.firstChild) {
+      this._ul.removeChild(this._ul.firstChild);
+    }
     this.is_open = false;
-  };
-};
+  }
+}
