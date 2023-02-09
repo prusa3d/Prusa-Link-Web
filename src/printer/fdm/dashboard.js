598fe16476f8798637dd9ba9f37847f3710601f4
Ilja Kartašov <ik@lowenware.com>
2023-02-09 13:59:58 +0100

    Leave only one camera on the dashboard

--- src/printer/fdm/dashboard.js
+++ src/printer/fdm/dashboard.js
@@ -11,12 +11,12 @@ import { LinkState } from "../../state";
 import { getJson } from "../../auth";
 
 const load = (context) => {
-  // TODO: request API for  the storage
   translate("home.link", { query: "#title-status-label" });
   graph.render();
   update(context);
   if (process.env['WITH_CAMERAS']) {
-    cameras.update(context, updateSnapshots);
+    cameras.update(context, null);
+    cameras.updateCurrentCamera();
   }
   getJson("/api/v1/storage")
     .then(result => {
@@ -31,66 +31,16 @@ const load = (context) => {
 };
 
 const update = (context) => {
+  
+  if (!context.printer) {
+    return;
+  }
   const linkState = LinkState.fromApi(context.printer.state);
   job.update(context);
   upload.update(linkState);
   if (process.env['WITH_CAMERAS']) {
-    cameras.update(context, updateSnapshots);
+    cameras.update(context, null);
   }
 };
-
-const updateSnapshots = (list, removed) => {
-  const listNode = document.getElementById("cameras-snapshots");
-
-  removed.forEach(camera => {
-    const cameraNode = cameras.getCameraNode(camera.id);
-    if (cameraNode) {
-      listNode.removeChild(cameraNode);
-    }
-  });
-
-  list.filter(
-    camera => camera.connected && !!camera.lastSnapshotUrl
-  ).sort(
-    (cam1, cam2) => cam1.lastSnapshotAt?.getTime() || 0 - cam2.lastSnapshotAt?.getTime() || 0
-  ).forEach(camera => {
-    const cameraNode = cameras.getCameraNode(camera.id);
-    if (cameraNode) {
-      updateCameraNode(cameraNode, camera);
-    } else {
-      const node = createCameraNode(camera);
-      if (node) {
-        listNode.appendChild(node);
-      }
-    }
-  });
-};
-
-const createCameraNode = (camera) => {
-  const template = document.getElementById("cameras-snapshots__item")?.content;
-  if (!template) {
-    return null;
-  }
-  const node = document.importNode(template, true);
-  const listItemNode = node.querySelector("li");
-  const nodeId = cameras.getCameraNodeId(camera.id);
-  listItemNode.id = nodeId;
-
-  const cameraId = camera.id;
-
-  listItemNode.addEventListener("click", (e) => {
-    cameras.updateCurrentCamera(cameraId);
-    e.preventDefault();
-  }, false);
-    
-  updateCameraNode(node, camera);
-
-  return node
-};
-
-const updateCameraNode = (node, camera) => {
-  const img = node.querySelector('img');
-  img.src = camera.lastSnapshotUrl;
-}
 
 export default { load, update };
